99a0fe153a61c62c53ced7ed2e91aac1a9c04444
nyamwaya <alecksonnyamwaya@gmail.com>
2024-03-18 22:56:18 -0500

    feat(repository): refactor

--- app/src/main/java/com/target/targetcasestudy/api/TargetApi.kt
+++ app/src/main/java/com/target/targetcasestudy/api/TargetApi.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Path
 
 internal const val BASE_URL = "https://api.target.com/mobile_case_study_deals/v1/"
 
-interface DealApi {
+interface TargetApi {
 
   @GET("${BASE_URL}deals")
   fun retrieveDeals(): Call<DealResponse>
