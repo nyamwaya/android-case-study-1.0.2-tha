6f7bd1a821d5cffc39a2c6b76aaaf1af89608ef7
nyamwaya <alecksonnyamwaya@gmail.com>
2024-03-18 22:59:33 -0500

    feat(repository): repository implementation

--- app/src/main/java/com/target/targetcasestudy/data/repository/DealsRepositoryImpl.kt
+++ app/src/main/java/com/target/targetcasestudy/data/repository/DealsRepositoryImpl.kt
@@ -1,9 +1,19 @@
 package com.target.targetcasestudy.data.repository
 
 import com.target.targetcasestudy.data.remote.TargetApi
+import com.target.targetcasestudy.data.remote.dto.DealsResponse
+import com.target.targetcasestudy.data.remote.dto.Product
 import com.target.targetcasestudy.domain.DealsRepository
 import javax.inject.Inject
 
 class DealsRepositoryImpl @Inject constructor(
     private val api: TargetApi
-) : DealsRepository
+) : DealsRepository {
+    override suspend fun getDeals(): DealsResponse {
+        return api.getDeals()
+    }
+
+    override suspend fun getDeals(id: String): Product {
+        return api.getSingleDeal(id)
+    }
+}
